064059486e0dd04d2144567a2e056d06401d0656
Vasco Nunes <vasco@vnws.uk>
2019-03-07 01:49:02 +0000

    Add new dist build

--- src/js/email.js
+++ src/js/email.js
@@ -1,5 +1,5 @@
-$("button#form-btn").click(function() {
-  event.preventDefault();
+$("button#form-btn").click(function(e) {
+  e.preventDefault();
   if ($('#email').val().length < 3 || $('#message').val().length < 10) {
     $('#form-error').text("Please fill both fields.")
     return
