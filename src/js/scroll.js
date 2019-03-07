5d0c8af7803d112c65c5db6d0c13ad2bfb0f9b9a
Vasco Nunes <vasco@vnws.uk>
2019-03-07 13:59:04 +0000

    Clean up code

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,28 +1,35 @@
-(function ($) {
-  'use strict' // Start of use strict
+(function($) {
+  'use strict'; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash)
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (
+      location.pathname.replace(/^\//, '') ===
+        this.pathname.replace(/^\//, '') &&
+      location.hostname === this.hostname
+    ) {
+      let target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 56)
-        }, 400)
-        return false
+        $('html, body').animate(
+          {
+            scrollTop: target.offset().top - 56,
+          },
+          400,
+        );
+        return false;
       }
-  }
-})
+    }
+  });
 
-      // Closes responsive menu when a scroll trigger link is clicked
-      $('.js-scroll-trigger').click(function () {
-        $('.navbar-collapse').collapse('hide')
-      })
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
 
-      // Activate scrollspy to add active class to navbar items on scroll
-      $('body').scrollspy({
-        target: '#main-nav',
-        offset: 56
-      })
-})(jQuery) // End of use strict
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#main-nav',
+    offset: 56,
+  });
+})(jQuery); // End of use strict
